c60f45127cbe666f446927059a1fcfc832795227
Rahul9370 <rthorat625@gmail.com>
2025-07-06 20:15:00 +0530

    Update vowel_consonant.py

--- Assignment 3/vowel_consonant.py
+++ Assignment 3/vowel_consonant.py
@@ -1,8 +1,8 @@
 # 2. Write a program to input any alphabet and check whether it is vowel or consonant.
 
-alphabet = input("Enter any Alphabet : ")
+alphabet = input("Enter any Alphabet : ").lower()    # lower convert all capital letter into small case 
 
-if(alphabet == 'a' or alphabet == 'e' or alphabet == 'i' or alphabet == 'o' or alphabet == 'u' ):
+if(alphabet in 'aeiou'):             #in ('aeiou') checks membership in the list of vowels.
     print("Alphabet is Vowel...")
 else:
     print("Alphabet is consonant...")
