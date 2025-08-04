22e6268bed66eafbbfffbdcfadb98ca7e4c473f0
Rahul9370 <rthorat625@gmail.com>
2025-08-04 12:56:22 +0530

    Update notes_given_amount.py

--- Assignment 5 (looping)/notes_given_amount.py
+++ Assignment 5 (looping)/notes_given_amount.py
@@ -1,35 +1,32 @@
 """5. Write a program to accept an integer amount from user and tell minimum
 number of notes needed for representing that amount. (Use looping to optimize
 the problem)"""
+amount = int(input("Enter total amount to distribute in minimum notes: "))
 
-amount = int(input("Enter total amount to distribute in minimum notes :"))
+note = 500
+total_notes = 0
 
-note500 = amount // 500
-re_amount = amount % 500
-print(re_amount)
-note200 = re_amount // 200
-re_amount %=  200                  # we can assign directle re_amount variable like this 
+while note >= 10:
+    count = amount // note
+    print("Notes for", note, ":", count)
+    total_notes += count
+    amount %= note  
 
-note100 = re_amount // 100
-re_amount = re_amount % 100
+    
+    if note == 500:
+        note = 200
+    elif note == 200:
+        note = 100
+    elif note == 100:
+        note = 50
+    elif note == 50:
+        note = 20
+    elif note == 20:
+        note = 10
+    elif note == 10:
+        break  
 
-note50 = re_amount // 50
-re_amount = re_amount % 50
+print("Minimum number of notes needed for representing that amount is:", total_notes)
 
-note20 = re_amount // 20
-re_amount = re_amount % 20
 
-note10 = re_amount // 10
-re_amount = re_amount % 10
-
-print("notes for 500:",note500)
-print("notes for 200:",note200)
-print("notes for 100:",note100)
-print("notes for 50:",note50)
-print("notes for 20:",note20)
-print("notes for 10:",note10)
-
-total_note = note500 + note200 + note100 + note50 + note20 + note10        #cal total notes needed for given amount
-
-print("Minimum number of notes needed for representing that amount is:",total_note)
     
