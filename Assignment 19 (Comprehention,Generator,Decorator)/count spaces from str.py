22405c0f5ee9c67e2139d9b896d1fcdcfde8bab2
Rahul9370 <rthorat625@gmail.com>
2026-04-10 22:48:21 +0530

    Delete Assignment 19 (Comprehention,Generator,Decorator) directory

--- Assignment 19 (Comprehention,Generator,Decorator)/count spaces from str.py
+++
@@ -1,5 +0,0 @@
-# 3. Count the number of spaces in a string (take input from user)
-
-mystring = input("Enter string :")
-spaces = sum(1 for x in mystring if x == " ")
-print("Total Spaces in s string is :",spaces)
--- Assignment 19 (Comprehention,Generator,Decorator)/remove vowel from strin.py
+++
@@ -1,5 +0,0 @@
-# 4. Remove all of the vowels in a string (take input from user)
-
-mystring = input("Enter string: ")
-remove_vowel = "".join([x for x in mystring if x.lower() not in "aeiou"])
-print("String without vowels:", remove_vowel)
