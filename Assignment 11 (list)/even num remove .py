200e158457fb13902246ff5341334b7a71eeecd9
Rahul9370 <rthorat625@gmail.com>
2026-04-10 22:45:36 +0530

    Delete Assignment 11 (list) directory

--- Assignment 11 (list)/even num remove .py
+++
@@ -1,7 +0,0 @@
-# 10. Write a program to print list after removing even numbers.
-
-mylist = [10, 85, 54, 77, 62, 74, 63, 57, 69]
-for i in mylist[:]:   # iterate over copy
-    if i % 2 == 0:
-        mylist.remove(i)
-print(mylist)
