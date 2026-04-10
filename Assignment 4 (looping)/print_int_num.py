4ee570f95a5fd69c9bd5d1c8b1bb536e2bbcec4c
Rahul9370 <rthorat625@gmail.com>
2026-04-10 22:52:50 +0530

    Delete Assignment 4 (looping) directory

--- Assignment 4 (looping)/print_int_num.py
+++
@@ -1,8 +0,0 @@
-# 7. WAP to print all integers upto n that aren’t divisible by 2 and 3.
-
-number = int(input("Enter Number :"))
-
-for i in range(1 , number+1):
-    if(i % 2 != 0 and i % 3 != 0):
-        print(i)
-    
