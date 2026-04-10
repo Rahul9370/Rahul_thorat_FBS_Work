cdd63d473d7172f2ace0a52bc37c493ffb065eed
Rahul9370 <rthorat625@gmail.com>
2026-04-10 23:20:34 +0530

    Delete Test1 directory

--- Test1/convert_km_to_meter_cm.py
+++
@@ -1,9 +0,0 @@
-# WAP to accept distance in km and convert it into  meters and centimeters both.
-
-kilometer = int(input("Enter a Kilometer:"))
-
-meter = 1000 * kilometer
-centimeter = 100000 * kilometer
-
-print(kilometer,"is equal to ",meter,"meters")
-print(kilometer,"is equal to ",centimeter,"centimeters")
