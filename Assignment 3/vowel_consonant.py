621aaaa9aa462ba8df708b7d43122c7c65475d18
Rahul9370 <rthorat625@gmail.com>
2026-04-10 22:52:26 +0530

    Delete Assignment 3 directory

--- Assignment 3/vowel_consonant.py
+++
@@ -1,8 +0,0 @@
-# 2. Write a program to input any alphabet and check whether it is vowel or consonant.
-
-alphabet = input("Enter any Alphabet : ").lower()    # lower convert all capital letter into small case 
-
-if(alphabet in 'aeiou'):             #in ('aeiou') checks membership in the list of vowels.
-    print("Alphabet is Vowel...")
-else:
-    print("Alphabet is consonant...")
